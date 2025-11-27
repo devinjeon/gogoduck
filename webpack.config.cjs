0228be9b1d9723b67b8eea5e0f1397e67d45ed24
Hyojun Jeon <gywns2702@gmail.com>
2025-11-28 05:22:10 +0900

    Add MiniCssExtractPlugin and CssMinimizerPlugin to webpack

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,6 +5,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
@@ -17,7 +19,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif|ico|m4a)$/i,
@@ -38,7 +40,17 @@ module.exports = {
                 { from: 'thumb.png', to: 'thumb.png' },
             ],
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
     ],
+    optimization: {
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
     devServer: {
         static: {
             directory: path.join(__dirname, 'dist'),
